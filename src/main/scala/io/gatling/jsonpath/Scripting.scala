60238774ab7fb869ee71fa4a1393fccea268ec32
Stephane Landelle <slandelle@excilys.com>
2013-11-01 00:14:29 +0100

    Verbose yet simple and very efficient

--- src/main/scala/io/gatling/jsonpath/Scripting.scala
+++ src/main/scala/io/gatling/jsonpath/Scripting.scala
@@ -3,56 +3,63 @@ package io.gatling.jsonpath
 // Comparison operators
 trait ComparisonOperator {
 
-	def isNumber(a: Any) = isIntegralNumber(a) || isFloatingPointNumber(a)
+	def compare[T: Ordering](lhs: T, rhs: T): Boolean
 
-	def isIntegralNumber(a: Any) = a.isInstanceOf[Int] || a.isInstanceOf[Long]
-	def asIntegralNumber(a: Any): Option[Long] = a match {
-		case a: Integer => Some(a.toLong)
-		case a: Long => Some(a)
-		case _ => None
-	}
-
-	def isFloatingPointNumber(a: Any) = a.isInstanceOf[Float] || a.isInstanceOf[Double]
-	def asFloatingPointNumber(a: Any): Option[Double] = a match {
-		case a: Int => Some(a.toDouble)
-		case a: Long => Some(a.toDouble)
-		case a: Float => Some(a.toDouble)
-		case a: Double => Some(a)
-		case _ => None
-	}
-
-	def compare[T <% Ordered[T]](lhs: T, rhs: T): Boolean
-
-	def apply(lhs: Any, rhs: Any): Boolean =
-		(lhs, rhs) match {
-			case (s1: String, s2: String) => compare(s1, s2)
-			case (i1, i2) if (isNumber(lhs) && isNumber(rhs)) =>
-				if (isIntegralNumber(lhs) && isIntegralNumber(rhs))
-					compare(asIntegralNumber(i1), asIntegralNumber(i2))
-				else
-					compare(asFloatingPointNumber(i1), asFloatingPointNumber(i2))
+	def apply(lhs: Any, rhs: Any): Boolean = lhs match {
+		case s1: String => rhs match {
+			case s2: String => compare(s1, s2)
 			case _ => false
 		}
+		case i1: Int => rhs match {
+			case i2: Int => compare(i1, i2)
+			case i2: Long => compare(i1, i2)
+			case i2: Double => compare(i1, i2)
+			case i2: Float => compare(i1, i2)
+			case _ => false
+		}
+		case i1: Long => rhs match {
+			case i2: Int => compare(i1, i2)
+			case i2: Long => compare(i1, i2)
+			case i2: Double => compare(i1, i2)
+			case i2: Float => compare(i1, i2)
+			case _ => false
+		}
+		case i1: Double => rhs match {
+			case i2: Int => compare(i1, i2)
+			case i2: Long => compare(i1, i2)
+			case i2: Double => compare(i1, i2)
+			case i2: Float => compare(i1, i2)
+			case _ => false
+		}
+		case i1: Float => rhs match {
+			case i2: Int => compare(i1, i2)
+			case i2: Long => compare(i1, i2)
+			case i2: Double => compare(i1, i2)
+			case i2: Float => compare(i1, i2)
+			case _ => false
+		}
+		case _ => false
+	}
 }
 
 object EqOperator extends ComparisonOperator {
-	override def compare[T <% Ordered[T]](lhs: T, rhs: T) = lhs == rhs
+	override def compare[T: Ordering](lhs: T, rhs: T) = lhs == rhs
 }
 
 object LessOperator extends ComparisonOperator {
-	override def compare[T <% Ordered[T]](lhs: T, rhs: T) = lhs < rhs
+	override def compare[T: Ordering](lhs: T, rhs: T) = implicitly[Ordering[T]].lt(lhs, rhs)
 }
 
 object GreaterOperator extends ComparisonOperator {
-	override def compare[T <% Ordered[T]](lhs: T, rhs: T) = lhs > rhs
+	override def compare[T: Ordering](lhs: T, rhs: T) = implicitly[Ordering[T]].gt(lhs, rhs)
 }
 
 object LessOrEqOperator extends ComparisonOperator {
-	override def compare[T <% Ordered[T]](lhs: T, rhs: T) = lhs <= rhs
+	override def compare[T: Ordering](lhs: T, rhs: T) = implicitly[Ordering[T]].lteq(lhs, rhs)
 }
 
 object GreaterOrEqOperator extends ComparisonOperator {
-	override def compare[T <% Ordered[T]](lhs: T, rhs: T) = lhs >= rhs
+	override def compare[T: Ordering](lhs: T, rhs: T) = implicitly[Ordering[T]].gteq(lhs, rhs)
 }
 
 // Binary boolean operators
